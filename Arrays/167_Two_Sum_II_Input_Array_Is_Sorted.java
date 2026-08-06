709752a32d20d3fcde3ad9c2e2ffa537dc0881d7
arulromeo18 <arulkumarv0811@gmail.com>
2026-08-06 12:25:21 +0530

    Implement twoSum method for sorted array

---
+++ Arrays/167_Two_Sum_II_Input_Array_Is_Sorted.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] ans = {-1, -1};
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+
+            int sum = nums[start] + nums[end];
+
+            if (sum == target) {
+                ans[0] = start + 1;
+                ans[1] = end + 1;
+                return ans;
+            }
+
+            if (sum < target) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+
+        return ans;
+    }
+}
